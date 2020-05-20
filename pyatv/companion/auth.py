69e13ed40d609041e91da0823c4b0fb7b0f8b4e0
Pierre Ståhl <pierre.staahl@gmail.com>
2020-05-20 22:06:51 +0200

    WIP: Start of pairing support for companion

---
+++ pyatv/companion/auth.py
@@ -0,0 +1,126 @@
+"""Device pairing and derivation of encryption keys.
+
+THIS IS PROTOTYPE AND DOES NOT WORK!
+"""
+
+import logging
+from enum import Enum
+from collections import namedtuple
+
+from pyatv.companion import opack
+from pyatv.support import log_binary, tlv8
+
+_LOGGER = logging.getLogger(__name__)
+
+Frame = namedtuple("Frame", "type data")
+
+
+class FrameType(Enum):
+    """Frame type values."""
+
+    Unknown = 0
+    NoOp = 1
+    PS_Start = 3
+    PS_Next = 4
+    PV_Start = 5
+    PV_Next = 6
+    U_OPACK = 7
+    E_OPACK = 8
+    P_OPACK = 9
+    PA_Req = 10
+    PA_Rsp = 11
+    SessionStartRequest = 16
+    SessionStartResponse = 17
+    SessionData = 18
+    FamilyIdentityRequest = 32
+    FamilyIdentityResponse = 33
+    FamilyIdentityUpdate = 34
+
+
+def decode_frame(data: bytes):
+    """Decode a frame from bytes."""
+    frame_type = FrameType(data[0])
+    length = (data[1] << 16) | (data[2] << 8) | data[3]
+    payload, _ = opack.unpack(data[4 : 4 + length])
+    payload["_pd"] = tlv8.read_tlv(payload["_pd"])
+    return Frame(frame_type, payload), data[4 + length :]
+
+
+def encode_frame(frame_type: FrameType, data: bytes):
+    """Encode a frame as bytes."""
+    payload = opack.pack(data)
+    header = bytes([frame_type.value]) + len(payload).to_bytes(3, byteorder="big")
+    return header + payload
+
+
+class CompanionPairingProcedure:
+    """Perform pairing and return new credentials."""
+
+    def __init__(self, connection, srp):
+        """Initialize a new MrpPairingHandler."""
+        self.connection = connection
+        self.srp = srp
+        self._atv_salt = None
+        self._atv_pub_key = None
+
+    # TODO: Should not be here
+    async def _send_and_receive(self, frame_type, message):
+        frame = encode_frame(frame_type, message)
+        self.connection.send(frame)
+        resp = await self.connection.read()
+        return decode_frame(resp)[0]
+
+    async def start_pairing(self):
+        """Start pairing procedure."""
+        self.srp.initialize()
+
+        await self.connection.connect()
+
+        msg = {
+            "_pd": tlv8.write_tlv({tlv8.TLV_METHOD: b"\x00", tlv8.TLV_SEQ_NO: b"\x01"}),
+            "_pwTy": 1,
+        }
+        resp = await self._send_and_receive(FrameType.PS_Start, msg)
+
+        pairing_data = resp.data["_pd"]
+        self._atv_salt = pairing_data[tlv8.TLV_SALT]
+        self._atv_pub_key = pairing_data[tlv8.TLV_PUBLIC_KEY]
+        log_binary(
+            _LOGGER,
+            "Got pub key and salt",
+            Salt=self._atv_salt,
+            PubKey=self._atv_pub_key,
+        )
+
+    async def finish_pairing(self, pin):
+        """Finish pairing process."""
+        self.srp.step1(pin)
+
+        pub_key, proof = self.srp.step2(self._atv_pub_key, self._atv_salt)
+
+        msg = {
+            "_pd": tlv8.write_tlv(
+                {
+                    tlv8.TLV_SEQ_NO: b"\x03",
+                    tlv8.TLV_PUBLIC_KEY: pub_key,
+                    tlv8.TLV_PROOF: proof,
+                }
+            )
+        }
+
+        resp = await self._send_and_receive(FrameType.PS_Next, msg)
+
+        pairing_data = resp.data["_pd"]
+        atv_proof = pairing_data[tlv8.TLV_PROOF]
+        log_binary(_LOGGER, "Device", Proof=atv_proof)
+
+        encrypted_data = self.srp.step3()
+        msg = {
+            "_pd": {tlv8.TLV_SEQ_NO: b"\x05", tlv8.TLV_ENCRYPTED_DATA: encrypted_data}
+        }
+        resp = await self._send_and_receive(FrameType.PS_Next, msg)
+
+        pairing_data = resp.data["_pd"]
+        encrypted_data = pairing_data[tlv8.TLV_ENCRYPTED_DATA]
+
+        return self.srp.step4(encrypted_data)
--- pyatv/companion/pairing.py
+++ pyatv/companion/pairing.py
@@ -1,11 +1,75 @@
 """Device pairing and derivation of encryption keys."""
 
+import asyncio
 import logging
 
+from pyatv import exceptions
 from pyatv.const import Protocol
 from pyatv.interface import PairingHandler
+from pyatv.companion.auth import CompanionPairingProcedure
+from pyatv.companion.srp import SRPAuthHandler
+from pyatv.support import error_handler, log_binary
 
 _LOGGER = logging.getLogger(__name__)
+
+
+# TODO: Temporary solution for a connection
+class CompanionConnection(asyncio.Protocol):
+    """Remote connection to a Companion device."""
+
+    def __init__(self, loop, host, port):
+        """Initialize a new CompanionConnection instance."""
+        self.loop = loop
+        self.host = str(host)
+        self.port = port
+        self.transport = None
+        self.semaphore = asyncio.Semaphore(value=0)
+        self.buffer = b""
+
+    async def connect(self):
+        """Connect to device."""
+        _LOGGER.debug("Connecting to Companion client")
+        await self.loop.create_connection(lambda: self, self.host, self.port)
+
+    def close(self):
+        """Close connection to device."""
+        _LOGGER.debug("Closing connection")
+        if self.transport:
+            self.transport.close()
+        self.transport = None
+
+    def send(self, data):
+        """Send data to companion."""
+        log_binary(_LOGGER, "Send data", Data=data)
+        self.transport.write(data)
+
+    async def read(self):
+        """Wait for data to be available and return it."""
+        await asyncio.wait_for(
+            self.semaphore.acquire(), timeout=3,
+        )
+        buffer = self.buffer
+        self.buffer = b""
+        return buffer
+
+    def connection_made(self, transport):
+        """Handle that connection was eatablished."""
+        _LOGGER.debug("Connected to companion device")
+        self.transport = transport
+
+    def data_received(self, data):
+        """Handle data received from companion."""
+        log_binary(_LOGGER, "Received data", Data=data)
+        self.buffer += data
+        self.semaphore.release()
+
+    def error_received(self, exc):
+        """Error received from companion."""
+        _LOGGER.debug("Error: %s", exc)
+
+    def connection_lost(self, exc):
+        """Handle that connection was lost from companion."""
+        _LOGGER.debug("Connection lost: %s", exc)
 
 
 class CompanionPairingHandler(PairingHandler):
@@ -14,24 +78,43 @@ class CompanionPairingHandler(PairingHandler):
     def __init__(self, config, session, loop):
         """Initialize a new CompanionPairingHandler."""
         super().__init__(session, config.get_service(Protocol.Companion))
+        self.connection = CompanionConnection(loop, config.address, self.service.port)
+        self.srp = SRPAuthHandler()
+        self.pairing_procedure = CompanionPairingProcedure(self.connection, self.srp)
         self.pin_code = None
+        self._has_paired = False
 
     async def close(self):
         """Call to free allocated resources after pairing."""
+        self.connection.close()
         await super().close()
 
     @property
     def has_paired(self):
         """If a successful pairing has been performed."""
-        return False
+        return self._has_paired
 
     async def begin(self):
         """Start pairing process."""
         _LOGGER.debug("Start pairing Companion")
+        await error_handler(
+            self.pairing_procedure.start_pairing, exceptions.PairingError
+        )
 
     async def finish(self):
         """Stop pairing process."""
         _LOGGER.debug("Finish pairing Companion")
+        if not self.pin_code:
+            raise exceptions.PairingError("no pin given")
+
+        self.service.credentials = str(
+            await error_handler(
+                self.pairing_procedure.finish_pairing,
+                exceptions.PairingError,
+                self.pin_code,
+            )
+        )
+        self._has_paired = True
 
     @property
     def device_provides_pin(self):
